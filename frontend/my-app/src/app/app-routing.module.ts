17cb33320a54e5a982c0071fe46349c63e13e96a
srcohen22 <samsupersam123@gmail.com>
2021-09-30 18:19:29 -0400

    Updated header and routing to include new pages

--- frontend/my-app/src/app/app-routing.module.ts
+++ frontend/my-app/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageComponent } from './page/page.component';
 import { ShippingComponent } from './shipping/shipping.component';
 import { PaymentComponent } from './payment/payment.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   { path: 'page', component: PageComponent} ,
   { path: 'shipping', component: ShippingComponent},
-  { path: 'payment', component: PaymentComponent}
+  { path: 'payment', component: PaymentComponent},
+  { path: 'aboutus', component: AboutUsComponent},
+  { path: 'contactus', component: ContactUsComponent},
+  { path: 'home', component: HomePageComponent}
 ];
 
 @NgModule({
