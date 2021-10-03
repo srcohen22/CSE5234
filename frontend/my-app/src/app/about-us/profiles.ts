f5079231869548317a742b9a8146d66121a8b9d7
srcohen22 <samsupersam123@gmail.com>
2021-10-03 18:26:32 -0400

    Built ContactUs page

--- frontend/my-app/src/app/about-us/profiles.ts
+++ frontend/my-app/src/app/about-us/profiles.ts
@@ -10,7 +10,7 @@ export const PROFILE: AboutUs[] = [
     image: '../../assets/cornchowder.jpg',
     position: 'CIO'},
   { name: 'Sam Cohen',
-    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+    description: 'Sam Cohen is a 4th year Computer and Information Science major, with a minor in Mathematics and Russian. He has absolutely no idea what he wants to do in the future when he graduates in 8 months, but he\'s hoping to figure it out before then. His hobbies include tennis, video games, music, and working on 5234 projects! He is a member of club tennis at OSU and finds it one of the most fun things he\'s done at OSU. He also thinks going to football games is extremely fun, despite not caring about football in the slightest. And of course, he has a passion for making homemade meals and buying and selling them on KitchenTwo...',
     image: '../../assets/carrotcake.jpg',
     position: 'Wage Slave'}
 ]
