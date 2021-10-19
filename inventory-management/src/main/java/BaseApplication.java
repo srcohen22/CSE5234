f42e575e9b3b80e07565f72069dffacee5a10151
gnoeth <32984013+gnoeth@users.noreply.github.com>
2021-10-19 18:04:44 -0400

    Delete inventory-management directory

--- inventory-management/src/main/java/BaseApplication.java
+++
@@ -1,8 +0,0 @@
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("")
-public class BaseApplication extends Application {
-}
